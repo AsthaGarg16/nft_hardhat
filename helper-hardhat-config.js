5849a4aef9e62f3d94211e2ba14f03b77223d9a9
AsthaGarg16 <asthagarg1611@gmail.com>
2022-10-10 23:19:39 +0800

    dynamic svg

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,6 +6,7 @@ const networkConfig = {
     1337: {
         name: "localhost",
         subscriptionId: "1007",
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         callbackGasLimit: "500000",
         mintFee: "10000000000000000", 
@@ -17,6 +18,7 @@ const networkConfig = {
         callbackGasLimit: "500000", // 500,000 gas
         mintFee: "10000000000000000",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        ethUsdPriceFeed:"0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
     },
     1: {
         name: "mainnet",
@@ -29,10 +31,12 @@ const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 const frontEndContractsFile = "../frontend/constants/contractAddresses.json"
 const frontEndAbiFile = "../frontend/constants/abi.json"
 
+const DECIMALS = "18"
+const INITIAL_PRICE = "200000000000000000000"
+
 module.exports = {
     networkConfig,
     developmentChains,
-    VERIFICATION_BLOCK_CONFIRMATIONS,
-    frontEndContractsFile,
-    frontEndAbiFile,
+    DECIMALS,
+    INITIAL_PRICE,
 }
