d0c9da9cae8b8eaeb0ba2d610eb706ade8034197
AsthaGarg16 <asthagarg1611@gmail.com>
2022-10-10 16:47:21 +0800

    random nft with pinata storage

--- hardhat.config.js
+++ hardhat.config.js
@@ -110,6 +110,9 @@ module.exports = {
                 version: "0.8.9",
             },
             {
+                version: "0.6.0",
+            },
+            {
                 version: "0.4.24",
             },
         ],
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,17 +7,15 @@ const networkConfig = {
         name: "localhost",
         subscriptionId: "1007",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-        keepersUpdateInterval: "30",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+        callbackGasLimit: "500000",
+        mintFee: "10000000000000000", 
     },
     5: {
         name: "goerli",
         subscriptionId: "1007",
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
-        keepersUpdateInterval: "30",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
+        mintFee: "10000000000000000",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     },
     1: {
